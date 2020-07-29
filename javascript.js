9c5d6bb64c69b4a3d9b22981abc7d0e3f08e6da2
shahdmadhoun <shahdem100@gmail.com>
2020-07-29 13:32:34 +0300

    start

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var typed = new Typed (".vide span.type" ,{
+    strings:[" "," Welcome all to our website",
+    "We deliver to all over the world" , "Be near from us !"],
+    loop:true,
+    typeSpeed:50,
+    backSpeed:50});
