64ed06329cc7a68f018b3f410983b2287ed12002
shahdmadhoun <shahdem100@gmail.com>
2020-08-01 14:00:25 +0300

    اضافة الاكسسوارات والاحذية

--- javascript.js
+++ javascript.js
@@ -4,3 +4,10 @@ var typed = new Typed (".vide span.type" ,{
     loop:true,
     typeSpeed:50,
     backSpeed:50});
+
+function myFunction() {
+    
+    document.getElementById("thank").innerHTML = "Thank you ";
+
+
+      }
